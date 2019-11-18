df65c737c637a8c456997b616b0d83ba3a26d0fa
Kylema <kylemaa95@gmail.com>
2019-11-18 10:52:34 -0800

    exported controllers

--- nodejs-react-app/src/comment/controllers/index.js
+++ nodejs-react-app/src/comment/controllers/index.js
@@ -1,1 +1,26 @@
-//TODO: Expose all the connectors/controllers 
+// Export all the connectors/controllers 
+import commentServices from '../use-cases/index'
+import makeDeleteComment from './delete-comment'
+import makeGetComment from './get-comment'
+import makePatchComment from './patch-comment'
+import makePostComment from './post-comment'
+
+// get the properties from the freeze object 
+const addComment = commentServices.addComment
+const editComment = commentServices.editComment
+const listComments = commentServices.listComments
+const removeComment = commentServices.removeComment
+
+// inject properties into controllers
+const deleteComment = makeDeleteComment({ removeComment })
+const getComments = makeGetComment({listComments})
+const postComment = makePostComment({ addComment })
+const patchComment = makePatchComment({ editComment })
+
+const commentController = Object.freeze({
+    deleteComment,
+    getComments,
+    postComment,
+    patchComment
+})
+export default commentController
--- nodejs-react-app/src/comment/use-cases/index.js
+++ nodejs-react-app/src/comment/use-cases/index.js
@@ -1,9 +1,9 @@
-//TODO: exports all the use cases with in this directory
+//exports all the use cases with in this directory
 import makeAddComment from './add-comment'
 import makeEditcomment from './edit-comment'
 import makeCommentList from './list-comment'
 import makeRemoveComment from './remove-comment'
-
+//TODO: database missing and handle for add and edit comment
 const addComment = makeAddComment({})
 const editComment = makeEditcomment({})
 const listComments = makeCommentList({})
