932deb718b9cc72fabd7be7612360ccccb5fd227
Kylema <kylemaa95@gmail.com>
2019-11-20 18:02:05 -0800

    added database findbyid method

--- nodejs-react-app/src/database/comment-db.js
+++ nodejs-react-app/src/database/comment-db.js
@@ -9,4 +9,17 @@ export default function makeCommentsDb ({ makeDb }) {
       remove,
       update
     })
+    /** use id of a comment to search for the comment within the collection */
+    async function findById({id:_id}){
+      const db = await makeDb()
+      result = await db.colllection('comment').find({_id})
+      // Returns an array of each value in this collection
+      found = await result.toArray()
+      if(found.length === 0 ){
+        return null
+      }
+      // copy the content of the key _id to id from array found and return the object
+      const {_id: id, ...info} = found[0]
+      return {id, ...info}
+    }
 }
