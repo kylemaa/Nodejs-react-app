1ff838bf9ecae1ae5efcc29fe0fad73783deced3
kylemaa <kylephuma@gmail.com>
2019-11-28 14:08:11 -0800

    some utility files for testing

---
+++ nodejs-react-app/__test__/fixtures/comment.js
@@ -0,0 +1,44 @@
+import faker from 'faker'
+
+const Id = Object.freeze({
+    makeId: cuid,
+    isValidId: cuid.isCuid
+  })
+
+function md5 (text) {
+    return crypto
+      .createHash('md5')
+      .update(text, 'utf-8')
+      .digest('hex')
+  }
+
+export default function makeFakeComment (overrides) {
+    const comment = {
+      author: faker.name.findName(),
+      createdOn: Date.now(),
+      id: Id.makeId(),
+      modifiedOn: Date.now(),
+      postId: Id.makeId(),
+      published: true,
+      replyToId: Id.makeId(),
+      text: faker.lorem.paragraph(3),
+      source: {
+        ip: faker.internet.ip(),
+        browser: faker.internet.userAgent(),
+        referrer: faker.internet.url()
+      }
+    }
+    //hash md5 
+    comment.hash = md5(
+        comment.text +
+          comment.published +
+          (comment.author || '') +
+          (comment.postId || '') +
+          (comment.replyToId || '')
+      )
+      
+    return {
+        ...comment,
+        ...overrides
+      }
+}
