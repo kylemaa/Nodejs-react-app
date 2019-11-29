acc8345600a1775fe7b9e573c18c99f437f7db1f
kylemaa <kylephuma@gmail.com>
2019-11-28 16:50:13 -0800

    trying to run jest on comment entity

--- nodejs-react-app/src/comment/comment-entity.spec.js
+++ nodejs-react-app/src/comment/comment-entity.spec.js
@@ -1,4 +1,10 @@
-xit ("must have an author", () => {})
+import makeComment from "./comment-entity"
+import makeFakeComment from "../../__test__/fixtures/comment"
+
+
+describe('comment', () => {
+it ("must have an author", () => {const comment = makeFakeComment({ author: null })
+expect(() => makeComment(comment)).toThrow('Comment must have an author.')})
 xit ("must have a valid post id", () => {})
 xit ("must have valid text", () => {})
 xit ("can be in reply to another comment", () => {})
@@ -15,3 +21,4 @@ xit ("must have a source", () => {})
 xit ("must have a source ip", () => {})
 xit ("can have a source browser", () => {})
 xit ("can have a source referrer", () => {})
+})
